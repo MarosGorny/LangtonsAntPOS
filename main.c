e711c8a6dbcabbeb00295835277bd09853b18103
Maroš Gorný <maros.gorny@gmail.com>
2022-12-17 01:23:23 +0100

    add inversion ant logic

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc,char* argv[]) {
     int areaWidth;
     int areaHeight;
     int areaSize;
-    bool directLogic = true;
+    bool directLogic = false;
 
     if(argc < 2) {
         areaSize = areaWidth = areaHeight = 11;
@@ -33,48 +33,46 @@ int main(int argc,char* argv[]) {
 
 
     for (int i = 0; i < 10; ++i) {
-        if(directLogic) {
-            BACKGROUND_COLOR antBoxColor = getBoxColorOfAnt(ant,(int *)display,areaWidth);
-            if(antBoxColor == WHITE) {
-                display[ant.x][ant.y] = BLACK;
-                switch (ant.direction) {
-                    case NORTH:
-                        ant.x++;
-                        break;
-                    case EAST:
-                        ant.y++;
-                        break;
-                    case SOUTH:
-                        ant.x--;
-                        break;
-                    case WEST:
-                        ant.y--;
-                        break;
-                    default:
-                        fprintf(stderr,"ant[%d] direction is not set\n",ant.id);
-                }
-                ant.direction = (ant.direction + 1) % 4;
-
-            } else if (antBoxColor == BLACK) {
-                display[ant.x][ant.y] = WHITE;
-                switch (ant.direction) {
-                    case NORTH:
-                        ant.x--;
-                        break;
-                    case EAST:
-                        ant.y--;
-                        break;
-                    case SOUTH:
-                        ant.x++;
-                        break;
-                    case WEST:
-                        ant.y++;
-                        break;
-                    default:
-                        fprintf(stderr,"ant[%d] direction is not set\n",ant.id);
-                }
-                ant.direction = (ant.direction + 3) % 4;
+        BACKGROUND_COLOR antBoxColor = getBoxColorOfAnt(ant,(int *)display,areaWidth);
+        if(antBoxColor == WHITE) {
+            display[ant.x][ant.y] = BLACK;
+            switch (ant.direction) {
+                case NORTH:
+                    ant.x += directLogic ? 1:-1;
+                    break;
+                case EAST:
+                    ant.y += directLogic ? 1:-1;
+                    break;
+                case SOUTH:
+                    ant.x += directLogic ? -1:1;
+                    break;
+                case WEST:
+                    ant.y += directLogic ? -1:1;
+                    break;
+                default:
+                    fprintf(stderr,"ant[%d] direction is not set\n",ant.id);
             }
+            ant.direction = directLogic ?  ((ant.direction + 1) % 4) : ((ant.direction + 3) % 4);
+
+        } else if (antBoxColor == BLACK) {
+            display[ant.x][ant.y] = WHITE;
+            switch (ant.direction) {
+                case NORTH:
+                    ant.x += directLogic ? -1:1;
+                    break;
+                case EAST:
+                    ant.y += directLogic ? -1:1;
+                    break;
+                case SOUTH:
+                    ant.x += directLogic ? 1:-1;
+                    break;
+                case WEST:
+                    ant.y += directLogic ? 1:-1;
+                    break;
+                default:
+                    fprintf(stderr,"ant[%d] direction is not set\n",ant.id);
+            }
+            ant.direction = directLogic ?  ((ant.direction + 3) % 4) : ((ant.direction + 1) % 4);
         }
         printAntInfo(ant, (int *)display, areaWidth);
     }
